6fd84728df71eb3a649bfff1896f922b800a2a6c
varvara <varvarashcherbyna7@gmail.com>
2023-03-02 19:02:14 +0200

    build: sign in start page

--- src/components/forms/sign_up_start_page/SignUpStartPage.tsx
+++ src/components/forms/sign_up_start_page/SignUpStartPage.tsx
@@ -1,10 +1,25 @@
+import { Box } from '@mui/material';
 import * as React from 'react';
+import SignUpStartPageCard from './card/SignUpStartPageCard';
+import style from './SignUpStartPage.module.sass';
 
 // eslint-disable-next-line no-unused-vars
 export interface ISignUpStartPage {}
 
 const SignUpStartPage: React.FC<ISignUpStartPage> = () => {
-  return <></>;
+  return (
+    <Box className={style.wrapper}>
+      <SignUpStartPageCard
+        title={'I AM A COACH'}
+        description={'I want to train and get paid easily.'}
+      />
+      <span className={style.orBox}> or </span>
+      <SignUpStartPageCard
+        title={'I AM A STUDENT'}
+        description={'I want to discover a coach and schedule a training.'}
+      />
+    </Box>
+  );
 };
 
 export default SignUpStartPage;
