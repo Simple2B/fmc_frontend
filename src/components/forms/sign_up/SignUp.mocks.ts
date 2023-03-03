f6c2592cbbe785fbb33c5baa83077654ab8c90e6
varvara <varvarashcherbyna7@gmail.com>
2023-03-03 19:21:41 +0200

    fix: sign up add tag

--- src/components/forms/sign_up/SignUp.mocks.ts
+++ src/components/forms/sign_up/SignUp.mocks.ts
@@ -3,6 +3,7 @@ import { ISignUp } from './SignUp';
 // eslint-disable-next-line no-unused-vars
 const base: ISignUp = {
   title: '',
+  signUpType: '',
 };
 
 export const mockBaseSignUpPageProps = {};
