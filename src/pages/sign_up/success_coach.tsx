e7a3f6d2cb6abb167d5edf1c961ac48ae8c5d9c5
varvara <varvarashcherbyna7@gmail.com>
2023-03-03 23:18:18 +0200

    build: success screen coach

---
+++ src/pages/sign_up/success_coach.tsx
@@ -0,0 +1,29 @@
+import SuccessSignUp from '@/components/success_sign_up/SuccessSignUp';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Head from 'next/head';
+import linkLogo from '../../../public/fmc_logo.png';
+import SignUpSPLayout from '../../components/layouts/sign_up_sp_layout/SignUpSPLayout';
+
+export default function SuccessCoach() {
+  const matches = useMediaQuery('(min-width:900px)');
+  return (
+    <>
+      <Head>
+        <title>Sign Up Coach</title>
+        <meta name="description" content="Sign Up Coach" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SignUpSPLayout
+        linkBackgroundImg={
+          matches ? '../../../img/young-basketball-bg-coach.png' : ' '
+        }
+        wrapperClassName={'boxWithBackgroundStudentPage'}
+        linkLogo={linkLogo}
+        color={'#000'}
+      >
+        <SuccessSignUp email={'test@email.com'} />
+      </SignUpSPLayout>
+    </>
+  );
+}
