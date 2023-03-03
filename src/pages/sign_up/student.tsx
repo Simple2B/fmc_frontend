29daf987c190da6326605a45f49dba827a546457
varvara <varvarashcherbyna7@gmail.com>
2023-03-03 14:40:54 +0200

    build: student page

---
+++ src/pages/sign_up/student.tsx
@@ -0,0 +1,29 @@
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Head from 'next/head';
+import linkLogo from '../../../public/fmc_logo.png';
+import SignUpStudent from '../../components/forms/sign_up/SignUp';
+import SignUpSPLayout from '../../components/layouts/sign_up_sp_layout/SignUpSPLayout';
+
+export default function SignUpCoachStudent() {
+  const matches = useMediaQuery('(min-width:900px)');
+  return (
+    <>
+      <Head>
+        <title>Sign Up Student</title>
+        <meta name="description" content="Sign Up Student" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SignUpSPLayout
+        linkBackgroundImg={
+          matches ? '../../../img/young-basketball-bg-student.jpg' : ' '
+        }
+        wrapperClassName={'boxWithBackgroundStudentPage'}
+        linkLogo={linkLogo}
+        color={'#000'}
+      >
+        <SignUpStudent title={'I’m a Student'} />
+      </SignUpSPLayout>
+    </>
+  );
+}
