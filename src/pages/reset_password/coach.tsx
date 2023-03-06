fc3b727001efb31a1af75cf3ae7cecb92d5b31cf
varvara <varvarashcherbyna7@gmail.com>
2023-03-06 10:03:03 +0200

    build: reset password pages for coach and student

---
+++ src/pages/reset_password/coach.tsx
@@ -0,0 +1,33 @@
+import { TypeSign, UserType } from '@/store/types/user';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Head from 'next/head';
+import linkLogo from '../../../public/fmc_logo.png';
+import ResetPassword from '../../components/forms/forgot_password/reset_password/ResetPassword';
+import SignUpSPLayout from '../../components/layouts/sign_up_sp_layout/SignUpSPLayout';
+
+export default function ResetPasswordCoach() {
+  const matches = useMediaQuery('(min-width:900px)');
+  return (
+    <>
+      <Head>
+        <title>Reset Password Coach</title>
+        <meta name="description" content="Reset Password Coach" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SignUpSPLayout
+        linkBackgroundImg={
+          matches ? '../../../img/pexels-shvets-production.png' : ' '
+        }
+        wrapperClassName={'boxWithBackgroundStudentPage'}
+        linkLogo={linkLogo}
+        color={'#000'}
+        description={'Don’t have an account?'}
+        userType={UserType.coach}
+        typeSign={TypeSign.in}
+      >
+        <ResetPassword userType={UserType.coach} />
+      </SignUpSPLayout>
+    </>
+  );
+}
