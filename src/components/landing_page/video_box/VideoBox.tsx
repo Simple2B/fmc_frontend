1907b06f1bde0c3b67ff0a4cd807441bb2cfc20a
varvara <varvarashcherbyna7@gmail.com>
2023-03-09 11:54:54 +0200

    build: responsive video component

--- src/components/landing_page/video_box/VideoBox.tsx
+++ src/components/landing_page/video_box/VideoBox.tsx
@@ -1,24 +1,13 @@
 import { Box } from '@mui/material';
 import * as React from 'react';
+import style from './VideoBox.module.sass';
 
 export interface IVideoBox {}
 
 const VideoBox: React.FC<IVideoBox> = () => {
   return (
-    <Box
-      sx={{
-        position: 'absolute',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '932px',
-        top: 0,
-        right: 0,
-        left: 0,
-      }}
-    >
-      <video autoPlay muted loop id="myVideo">
+    <Box className={style.wrapperVideo}>
+      <video autoPlay muted loop id="myVideo" className={style.video}>
         <source
           src={
             'https://find-my-coach.s3.amazonaws.com/assets/pexels-cottonbro-10340715.mp4'
