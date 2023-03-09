5b904b467b45a8d78a2c2f5e5755ba28ce33174f
varvara <varvarashcherbyna7@gmail.com>
2023-03-09 22:41:17 +0200

    build: main section

--- src/components/landing_page/main_section/MainSection.tsx
+++ src/components/landing_page/main_section/MainSection.tsx
@@ -1,7 +1,10 @@
 import { Box } from '@mui/material';
 import TitleBox from '../title_box/TitleBox';
-import Cards from './cards/Cards';
+import Cards from './cards_sport_types/Cards';
+import CardsTypesPayment from './cards_type_payment/CardsTypesPayment';
+import InfoBoxSignUp from './info_box_sign_up/InfoBoxSignUp';
 import styles from './MainSection.module.sass';
+import QuestionsCards from './questions_cards/QuestionsCards';
 
 export interface IMainSection {}
 
@@ -11,11 +14,41 @@ const MainSection: React.FC<IMainSection> = () => {
       <TitleBox
         color={'#000'}
         top={''}
-        title={'Explore sport categories '}
+        title={'Explore sport categories'}
         description={'Choose your coach based on your favorite sport'}
       />
       <Box className={styles.wrapperCardTypeSports}>
         <Cards />
+      </Box>
+      <TitleBox
+        color={'#000'}
+        top={''}
+        title={'How it works'}
+        description={
+          'We make it fast, simple and secure to book a coach that can help increase your potential.'
+        }
+      />
+      <Box className={styles.wrapperCardTypePayment}>
+        {/* <Box className={styles.arrow1}> - </Box> */}
+        <CardsTypesPayment />
+        {/* <Box className={styles.arrow2}> - </Box> */}
+      </Box>
+      <Box className={styles.wrapperCardTypePayment}>
+        <InfoBoxSignUp />
+      </Box>
+      <TitleBox
+        color={'#000'}
+        top={''}
+        title={'General FAQs'}
+        description={'Everything you have to know about the platform'}
+      />
+      <Box className={styles.wrapperComputerImg}>
+        <Box className={styles.wrapperBackground} />
+        {/* TODO: add img computer */}
+        <Box></Box>
+      </Box>
+      <Box className={styles.wrapperQuestion}>
+        <QuestionsCards />
       </Box>
     </Box>
   );
