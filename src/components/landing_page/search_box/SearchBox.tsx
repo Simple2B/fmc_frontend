3ff0f703de3e09d8a050ae10bf06c12c400a0885
varvara <varvarashcherbyna7@gmail.com>
2023-03-09 11:54:35 +0200

    build: responsive search component

--- src/components/landing_page/search_box/SearchBox.tsx
+++ src/components/landing_page/search_box/SearchBox.tsx
@@ -1,5 +1,5 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { Box, Button } from '@mui/material';
+import { Box, Button, useMediaQuery } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase';
@@ -8,39 +8,47 @@ import Paper from '@mui/material/Paper';
 export interface ISearchInput {}
 
 const SearchInput: React.FC<ISearchInput> = () => {
+  const matches845 = useMediaQuery('(max-width:845px)');
+  const matches445 = useMediaQuery('(max-width:445px)');
+  const matches320 = useMediaQuery('(max-width:320px)');
   return (
     <Paper
       component="form"
       sx={{
-        p: '2px 4px',
+        p: matches320 ? '' : '2px 4px',
         display: 'flex',
         alignItems: 'center',
-        width: 587,
-        height: 60,
+        width: matches445 ? 300 : matches845 ? 400 : 587,
+        height: matches445 ? 33 : matches845 ? 45 : 60,
         position: 'relative',
+        left: matches320 ? '-6px' : 0,
+        backgroundColor: matches320
+          ? 'rgba(255, 255, 255, 0.7)'
+          : 'rgba(255, 255, 255, 1)',
       }}
     >
       <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
         <SearchIcon />
       </IconButton>
       <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search a coach, sport"
+        sx={{ ml: 1, flex: 1, fontSize: matches845 ? '12px' : '' }}
+        placeholder={matches445 ? 'Search' : 'Search a coach, sport'}
         inputProps={{ 'aria-label': 'search google maps' }}
       />
 
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <Box sx={{ pr: '330px' }}>
+      <Box sx={{ pr: matches445 ? '150px' : matches845 ? '210px' : '330px' }}>
         <Button
           sx={{
             position: 'absolute',
-            top: '7px',
+            textTransform: 'capitalize',
+            top: matches845 ? '4.5px' : '7px',
             right: '6px',
             color: '#FFF',
             backgroundColor: '#F05547',
-            width: '108px',
-            height: '46px',
-            borderRadius: '8px',
+            width: matches445 ? '' : matches845 ? '88px' : '108px',
+            height: matches445 ? '24px' : matches845 ? '26px' : '46px',
+            borderRadius: matches845 ? '4px' : '8px',
             '&:hover': {
               backgroundColor: 'grey',
             },
