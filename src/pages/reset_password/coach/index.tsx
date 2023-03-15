def745f4fe63c3204223b25c756bbd1a9fc133f9
varvara <varvarashcherbyna7@gmail.com>
2023-03-15 12:23:19 +0200

    build: reset password pages

--- src/pages/reset_password/coach/index.tsx
+++ src/pages/reset_password/coach/index.tsx
@@ -1,9 +1,9 @@
 import { TypeSign, UserType } from '@/store/types/user';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Head from 'next/head';
+import ResetPassword from '../../../components/forms/forgot_password/reset_password/ResetPassword';
+import SignUpSPLayout from '../../../components/layouts/sign_up_sp/SignUpSPLayout';
 import linkLogo from '../../../public/LOGO.svg';
-import ResetPassword from '../../components/forms/forgot_password/reset_password/ResetPassword';
-import SignUpSPLayout from '../../components/layouts/sign_up_sp/SignUpSPLayout';
 
 export default function ResetPasswordCoach() {
   const matches = useMediaQuery('(min-width:900px)');
