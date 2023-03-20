d36942eafe69f77606bbf7b2fc31dbf1566ff954
varvara <varvarashcherbyna7@gmail.com>
2023-03-20 19:54:44 +0200

    build: component settings

--- src/components/profiles/student/settings/Settings.tsx
+++ src/components/profiles/student/settings/Settings.tsx
@@ -1,37 +1,24 @@
+import ChangePassword from '@/components/forms/change_password/ChangePassword';
+import PersonalInfo from '@/components/forms/persona_iInfo/PersonalInfo';
 import Box from '@mui/material/Box';
 import * as React from 'react';
-import ChangePassword from './forms/ChangePassword';
-import PersonalInfo from './forms/PersonalInfo';
 import styles from './Settings.module.sass';
 
-export interface ISettings {}
+export interface ISettings {
+  userType: string;
+}
 
-const Settings: React.FC<ISettings> = () => {
+const Settings: React.FC<ISettings> = ({ userType }) => {
   // const router = useRouter();
 
   return (
     <Box className={styles.wrapper}>
-      <Box
-        sx={{
-          mt: '28px',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Box
-          sx={{
-            maxWidth: '998.27px',
-            p: '23px 60px',
-            boxShadow: '0px 0px 15px rgba(199, 199, 199, 0.25)',
-            borderRadius: '8px',
-          }}
-        >
+      <Box className={styles.container}>
+        <Box className={styles.personalInfoContainer}>
           <PersonalInfo />
         </Box>
-        <Box sx={{ maxWidth: '998.27px', p: '23px 60px' }}>
-          <ChangePassword />
+        <Box className={styles.changePasswordContainer}>
+          <ChangePassword userType={userType} />
         </Box>
       </Box>
     </Box>
