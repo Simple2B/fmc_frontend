4313f7e91a753d3d351501d6cc627fe41bdccc8b
varvara <varvarashcherbyna7@gmail.com>
2023-03-25 17:32:39 +0200

    fix: checkbox default value

--- src/components/forms/your_profile/YourProfile.tsx
+++ src/components/forms/your_profile/YourProfile.tsx
@@ -275,7 +275,6 @@ const YourProfile: React.FC<IYourProfile> = ({ userType }) => {
               control={
                 <Checkbox
                   className={styles.checkbox}
-                  defaultChecked
                   checked={checkedAdults}
                   onChange={() => {
                     setCheckedAdults(!checkedAdults);
