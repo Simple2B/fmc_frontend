d3c56cfd582b0c1ef73c2309fa4ccfe0659bc60d
varvara <varvarashcherbyna7@gmail.com>
2023-03-28 13:41:53 +0300

    fix: modal window position

--- src/common/modal/Modal.tsx
+++ src/common/modal/Modal.tsx
@@ -23,14 +23,14 @@ const CustomModel: React.FC<ICustomModel> = ({
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
       sx={{
-        height: '100vh',
-        width: '100vw',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        // margin: '15% auto',
+        // height: '100vh',
+        // width: '100vw',
+        // position: 'absolute',
+        // top: 0,
+        // left: 0,
+        // right: 0,
+        // bottom: 0,
+        // // margin: '15% auto',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
