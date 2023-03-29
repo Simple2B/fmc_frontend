ef293e7bf2b886bfe0236ca59ee7aad4d56e8d14
varvara <varvarashcherbyna7@gmail.com>
2023-03-29 09:56:23 +0300

    build: add re for city and street

--- src/components/forms/your_profile/YourProfile.tsx
+++ src/components/forms/your_profile/YourProfile.tsx
@@ -6,7 +6,12 @@ import CustomModel from '@/common/modal/Modal';
 import DragDropFiles from '@/common/upload_drag_and_drop_input/DragDropFiles';
 import { coachProfileApi } from '@/fast_api_backend/api/usersInstance/coach/profileInstance';
 import { getErrorMessage } from '@/helper/error_function';
-import { emptyLocation, RE_POST_CODE } from '@/store/constants';
+import {
+  emptyLocation,
+  RE_CITY,
+  RE_POST_CODE,
+  RE_STREET,
+} from '@/store/constants';
 import { ILocationUI, TypeLocation } from '@/store/types/location/locationType';
 import { ISport } from '@/store/types/sport/sportType';
 import { UserType } from '@/store/types/user';
@@ -199,6 +204,18 @@ const YourProfile: React.FC<IYourProfileCoach> = ({ userType }) => {
             if (
               typeLocation === TypeLocation.postal_code &&
               !RE_POST_CODE.test(e.target.value.toLowerCase())
+            ) {
+              return item;
+            }
+            if (
+              typeLocation === TypeLocation.city &&
+              !RE_CITY.test(e.target.value.toLowerCase())
+            ) {
+              return item;
+            }
+            if (
+              typeLocation === TypeLocation.street &&
+              !RE_STREET.test(e.target.value.toLowerCase())
             ) {
               return item;
             }
