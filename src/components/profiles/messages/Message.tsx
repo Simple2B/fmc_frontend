653fb318a32330c49c84eeb59699f763b9a06260
Taraslysak <lysak.ipr@gmail.com>
2023-03-29 12:46:26 +0300

    fix: message style

--- src/components/profiles/messages/Message.tsx
+++ src/components/profiles/messages/Message.tsx
@@ -16,65 +16,51 @@ export function Message({ text, isOutgoing, date }: IMessageProps) {
     <Box
       sx={{
         display: 'flex',
-        flexDirection: 'column',
-        alignItems: isOutgoing ? 'flex-end' : 'flex-start',
-        width: '100%',
-        height: 'auto',
+        justifyContent: isOutgoing ? 'flex-end' : 'flex-start',
+        margin: '0.25rem 1rem ',
       }}
     >
       <Box
         sx={{
-          margin: 'auto 1%',
-          display: 'flex',
-          minWidth: '10%',
-          maxHeight: '100%',
-          maxWidth: 'auto',
-          width: 'auto',
           height: 'auto',
+          padding: '0.75rem',
+          color: 'black',
+          backgroundColor: '#d2d4f9',
+          borderRadius: '0.5rem ',
+          minWidth: '10rem',
+          maxWidth: '60%',
         }}
       >
-        <Box
-          sx={{
-            width: '100%',
-            height: 'auto',
-            padding: '5%',
-            color: 'black',
-            backgroundColor: '#d2d4f9',
-            borderRadius: '0.5rem / 3rem 3rem',
-            marginLeft: '2%',
-            marginTop: '5%',
+        <Typography
+          fontFamily={'Inter'}
+          fontSize={{
+            lg: 16,
+            md: 10,
+            sm: 10,
+            xs: 8,
           }}
+          lineHeight={'normal'}
+          style={{ wordWrap: 'break-word' }}
         >
+          {text}
+        </Typography>
+        <Box>
           <Typography
             fontFamily={'Inter'}
+            fontWeight={'600'}
+            color={'#9E9E9E'}
             fontSize={{
-              lg: 16,
-              md: 10,
-              sm: 10,
-              xs: 8,
+              lg: 10,
+              md: 8,
+              sm: 7,
+              xs: 6,
             }}
-            lineHeight={'normal'}
+            marginTop={'2.5%'}
           >
-            {text}
+            {messageDate}
+            <br />
+            {messageTime}
           </Typography>
-          <Box>
-            <Typography
-              fontFamily={'Inter'}
-              fontWeight={'600'}
-              color={'#9E9E9E'}
-              fontSize={{
-                lg: 10,
-                md: 8,
-                sm: 7,
-                xs: 6,
-              }}
-              marginTop={'2.5%'}
-            >
-              {messageDate}
-              <br />
-              {messageTime}
-            </Typography>
-          </Box>
         </Box>
       </Box>
     </Box>
