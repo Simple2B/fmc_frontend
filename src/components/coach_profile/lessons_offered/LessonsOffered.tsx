887da0a8d86221068c0d10194cd3ebc5d49a1e6a
varvara <varvarashcherbyna7@gmail.com>
2023-03-31 09:43:55 +0300

    build: lesson offered card

--- src/components/coach_profile/lessons_offered/LessonsOffered.tsx
+++ src/components/coach_profile/lessons_offered/LessonsOffered.tsx
@@ -1,6 +1,56 @@
 import { Box } from '@mui/material';
 import * as React from 'react';
-import LessonsOfferedCards from './_LessonsOfferedCard';
+import LessonsOfferedCards from './lessons_offere_card/LessonsOfferedCard';
+
+const testDataOneToOne = {
+  title: '1-on-1 Tennis Lessons',
+  description:
+    'Tincidunt commodo eu curabitur interdum lacinia ullamcorper purus. Eleifend eget accumsan in donec maecenas tempor. ',
+  itemsDescription: [
+    '60 minutes of tennis training',
+    'Tennis equipment provided',
+    'Tennis equipment provided',
+    '60 minutes of tennis training',
+    '60 minutes of tennis training',
+    'Tennis equipment provided',
+    'Tennis equipment provided',
+  ],
+  location: [
+    {
+      name: 'Reagal Tennis Club',
+      address: '64 Deer Ridge Dr., Santa Cruz, CA 10001 (map)',
+    },
+    {
+      name: 'Green Tennis Club',
+      address: '64 Alder St., Santa Cruz, CA 10001',
+    },
+  ],
+};
+
+const testDataGroup = {
+  title: 'Group Tennis Lesson',
+  description:
+    'Tincidunt commodo eu curabitur interdum lacinia ullamcorper purus. Eleifend eget accumsan in donec maecenas tempor. ',
+  itemsDescription: [
+    '60 minutes of tennis training',
+    'Tennis equipment provided',
+    'Tennis equipment provided',
+    '60 minutes of tennis training',
+    '60 minutes of tennis training',
+    'Tennis equipment provided',
+    'Tennis equipment provided',
+  ],
+  location: [
+    {
+      name: 'Reagal Tennis Club',
+      address: '64 Deer Ridge Dr., Santa Cruz, CA 10001 (map)',
+    },
+    {
+      name: 'Green Tennis Club',
+      address: '64 Alder St., Santa Cruz, CA 10001',
+    },
+  ],
+};
 
 export interface ILessonsOffered {}
 
@@ -33,7 +83,21 @@ const LessonsOffered: React.FC<ILessonsOffered> = () => {
         {' '}
         LessonsOffered
       </Box>
-      <LessonsOfferedCards />
+      <LessonsOfferedCards
+        title={testDataOneToOne.title}
+        description={testDataOneToOne.description}
+        itemsDescription={testDataOneToOne.itemsDescription}
+        location={testDataOneToOne.location}
+        // children={}
+      />
+
+      <LessonsOfferedCards
+        title={testDataGroup.title}
+        description={testDataGroup.description}
+        itemsDescription={testDataGroup.itemsDescription}
+        location={testDataGroup.location}
+        // children={}
+      />
     </Box>
   );
 };
