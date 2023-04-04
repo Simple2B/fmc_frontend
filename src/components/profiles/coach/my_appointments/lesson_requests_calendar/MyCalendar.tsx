f7605b5b5d9c62f70e88f37bb4f9bd70379a37ac
varvara <varvarashcherbyna7@gmail.com>
2023-04-04 10:14:25 +0300

    build: calendar packeges

--- src/components/profiles/coach/my_appointments/lesson_requests_calendar/MyCalendar.tsx
+++ src/components/profiles/coach/my_appointments/lesson_requests_calendar/MyCalendar.tsx
@@ -1,10 +1,38 @@
 import Box from '@mui/material/Box';
+import format from 'date-fns/format';
+import getDay from 'date-fns/getDay';
+import enUS from 'date-fns/locale/en-US';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
 import * as React from 'react';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+
+const locales = {
+  'en-US': enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 export interface IMyCalendar {}
 
 const MyCalendar: React.FC<IMyCalendar> = () => {
-  return <Box>My Calendar</Box>;
+  return (
+    <Box>
+      <Calendar
+        localizer={localizer}
+        // events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </Box>
+  );
 };
 
 export default MyCalendar;
