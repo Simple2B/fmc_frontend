e176f95c9586a8ee63603fbe14e4e2d96f47ebc4
BohdanSizomin <bohdansizomin@gmail.com>
2023-04-12 18:25:15 +0300

    feat: stripe connect logic

--- src/services/services/LessonService.ts
+++ src/services/services/LessonService.ts
@@ -9,62 +9,62 @@ import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
 
 export class LessonService {
+  /**
+   * Get Lessons For Student
+   * @returns StudentLesson Successful Response
+   * @throws ApiError
+   */
+  public static apiGetLessonsForStudent(): CancelablePromise<
+    Array<StudentLesson>
+  > {
+    return __request(OpenAPI, {
+      method: 'GET',
+      url: '/api/lesson/lessons/student',
+    });
+  }
 
-    /**
-     * Get Lessons For Student
-     * @returns StudentLesson Successful Response
-     * @throws ApiError
-     */
-    public static apiGetLessonsForStudent(): CancelablePromise<Array<StudentLesson>> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/lesson/lessons/student',
-        });
-    }
+  /**
+   * Get Upcoming Lessons
+   * @returns StudentLessonList Successful Response
+   * @throws ApiError
+   */
+  public static apiGetUpcomingLessons(): CancelablePromise<StudentLessonList> {
+    return __request(OpenAPI, {
+      method: 'GET',
+      url: '/api/lesson/lessons/student/upcoming',
+    });
+  }
 
-    /**
-     * Get Upcoming Lessons
-     * @returns StudentLessonList Successful Response
-     * @throws ApiError
-     */
-    public static apiGetUpcomingLessons(): CancelablePromise<StudentLessonList> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/lesson/lessons/student/upcoming',
-        });
-    }
+  /**
+   * Get Upcoming Appointments
+   * @returns StudentLessonList Successful Response
+   * @throws ApiError
+   */
+  public static apiGetUpcomingAppointments(): CancelablePromise<StudentLessonList> {
+    return __request(OpenAPI, {
+      method: 'GET',
+      url: '/api/lesson/lessons/coach/upcoming',
+    });
+  }
 
-    /**
-     * Get Upcoming Appointments
-     * @returns StudentLessonList Successful Response
-     * @throws ApiError
-     */
-    public static apiGetUpcomingAppointments(): CancelablePromise<StudentLessonList> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/lesson/lessons/coach/upcoming',
-        });
-    }
-
-    /**
-     * Get Lesson
-     * @param lessonUuid
-     * @returns StudentLesson Successful Response
-     * @throws ApiError
-     */
-    public static apiGetLesson(
-        lessonUuid: string,
-    ): CancelablePromise<StudentLesson> {
-        return __request(OpenAPI, {
-            method: 'GET',
-            url: '/api/lesson/{lesson_uuid}',
-            path: {
-                'lesson_uuid': lessonUuid,
-            },
-            errors: {
-                422: `Validation Error`,
-            },
-        });
-    }
-
+  /**
+   * Get Lesson
+   * @param lessonUuid
+   * @returns StudentLesson Successful Response
+   * @throws ApiError
+   */
+  public static apiGetLesson(
+    lessonUuid: string | undefined | string[]
+  ): CancelablePromise<StudentLesson> {
+    return __request(OpenAPI, {
+      method: 'GET',
+      url: '/api/lesson/{lesson_uuid}',
+      path: {
+        lesson_uuid: lessonUuid,
+      },
+      errors: {
+        422: `Validation Error`,
+      },
+    });
+  }
 }
