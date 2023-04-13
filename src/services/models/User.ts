b4ed07bf829b3208169a394b9919ff1ec73ac66b
BohdanSizomin <bohdansizomin@gmail.com>
2023-04-13 16:47:30 +0300

    feat: stripe connect cases

--- src/services/models/User.ts
+++ src/services/models/User.ts
@@ -3,12 +3,12 @@
 /* eslint-disable */
 
 export type User = {
-    uuid?: string;
-    email: string;
-    username: string;
-    first_name: string;
-    last_name: string;
-    is_verified: boolean;
-    profile_picture?: string;
+  uuid?: string;
+  email: string;
+  username: string;
+  first_name: string;
+  last_name: string;
+  is_verified: boolean;
+  profile_picture?: string;
+  stripe_account_id: string | null;
 };
-
