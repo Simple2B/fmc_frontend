d05a026bef0f146eb6f987685eb9d9c30470a70c
HC898 <96181034+HC898@users.noreply.github.com>
2023-05-31 20:19:53 +0100

    Update MainSection.tsx

--- src/components/landing_page/main_section/MainSection.tsx
+++ src/components/landing_page/main_section/MainSection.tsx
@@ -21,7 +21,7 @@ const typeQuestions = [
   {
     question: 'How much does it cost to use FindMyCoach?',
     answer:
-      'For coaches it`s FREE. Athletes pay a very small charge for bookings (3.9% + 20p) which includes the Stripe payments fee and covers the cost of the Stripe Express accounts which you will have access to. We will have subscription tiers as we include some of our exciting new features but don’t worry - we will always have the option to keep using our platform for no subscription cost.',
+      'For coaches it`s completely free to sign up and there are no booking fees!',
     isOpen: false,
     isShowArrow: false,
   },
